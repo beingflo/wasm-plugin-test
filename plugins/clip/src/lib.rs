242f4ab5b496714cde792f0bec1c7fb507a96025
Florian Marending <florian@marending.dev>
2023-12-01 16:10:46 +0100

    Modify clip plugin

--- plugins/clip/src/lib.rs
+++ plugins/clip/src/lib.rs
@@ -1,5 +1,4 @@
 use extism_pdk::*;
-use json::Number;
 use serde_json::Value;
 
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -13,8 +12,8 @@ pub fn clip(Json(mut data): Json<Vec<MetricRow>>) -> FnResult<Json<Vec<MetricRow
     for d in data.iter_mut() {
         let this_value = d.data["co2"].as_number().unwrap().as_i64().unwrap();
 
-        if this_value > 850 {
-            d.data["co2"] = Value::from(Number::from(0));
+        if this_value > 750 && this_value < 800 {
+            d.data["co2"] = Value::Null;
         }
     }
     Ok(Json(data))
